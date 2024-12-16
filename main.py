7a8395f4e636c49a094b6492dd18290c9ce23e0a
Jake <jbruder92786@gmail.com>
2024-12-16 11:08:55 -0700

    Player moves left, right, forward and backwards now

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import pygame
-from constants import * #import SCREEN_WIDTH, SCREEN_HEIGHT # This will import the values
+from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
-    # Start the game loop
-    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                return 
             
-        screen.fill((0, 0, 0))
-
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-                    
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        
 
-    
-    
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
